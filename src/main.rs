e43b409f258993681f79f62bbff728294817a33b
Chris Zimmerman <chris@chriszimmerman.net>
2021-11-07 14:08:32 -0500

    Adds player to map

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod map;
+mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -9,14 +10,21 @@ mod prelude {
 
 use prelude::*;
 use crate::map::Map;
+use crate::player::Player;
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        Self {
+            map: Map::new(),
+            player: Player::new(
+                Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+            ),
+        }
     }
 }
 
@@ -24,7 +32,9 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
 
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
