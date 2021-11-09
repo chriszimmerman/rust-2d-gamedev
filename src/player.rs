d109ffa80a8ac348d515e76d7a47cf401fa06d4f
Chris Zimmerman <chris@chriszimmerman.net>
2021-11-08 19:39:26 -0500

    Uses tileset to render map. Focuses camera on player

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,4 @@
 use crate::prelude::*;
-use crate::map::Map;
 
 pub struct Player {
     pub position: Point
@@ -12,17 +11,18 @@ impl Player {
         }
     }
 
-    pub fn render(&self, ctx: &mut BTerm) {
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(1);
         ctx.set(
-            self.position.x,
-            self.position.y,
+            self.position.x - camera.left_x,
+            self.position.y - camera.top_y,
             WHITE,
             BLACK,
             to_cp437('@')
         )
     }
 
-    pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
+    pub fn update(&mut self, ctx: &mut BTerm, map: &Map, camera: &mut Camera) {
         if let Some(key) = ctx.key {
             let delta = match key {
                 VirtualKeyCode::Left => Point::new(-1, 0),
@@ -35,7 +35,8 @@ impl Player {
             let new_position = self.position + delta;
 
             if map.can_enter_tile(new_position) {
-                self.position = new_position
+                self.position = new_position;
+                camera.on_player_move(new_position);
             }
         }
     }
